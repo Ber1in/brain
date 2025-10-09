f4446399f2664dbacc07ac91a1abd4a5159aa0cb
wubl <wubl@yunsilicon.com>
2025-10-09 09:52:47 +0800

    Update install scripts

--- install/install.sh
+++ install/install.sh
@@ -12,14 +12,23 @@ fi
 
 pip3 install $filename
 
-echo "=== 2. install brain service ==="
-\cp -f brain.service /usr/lib/systemd/system/
+echo "=== 2. install brain-server service ==="
+\cp -f brain-server.service /usr/lib/systemd/system/
 
-echo "=== 3. enable service auto start"
-systemctl enable brain
+echo "=== 3. enable  barin-server service auto start"
+systemctl enable brain-server
 
-systemctl restart brain
+systemctl restart brain-server
 
-echo "=== install completed ==="
+echo "=== install brain-server completed ==="
 
+
+if ! command -v node &> /dev/null; then
+    echo "install Node.js..."
+    mkdir -p /usr/local/node
+    tar -xJf node-v20.19.5-linux-x64.tar.xz -C /usr/local/node --strip-components=1
+
+    export PATH=/usr/local/node/bin:$PATH
+    echo 'export PATH=/usr/local/node/bin:$PATH' >> ~/.bashrc
+fi
 
