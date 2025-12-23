c3c5a92e4ef58cbbb84dbd1c046aeea57ecfc745
wubl <wubl@yunsilicon.com>
2025-12-23 14:16:12 +0800

    Fix: The installation package for the lldpctl command is lldpd

--- brain-backend/brain/utils/common_utils.py
+++ brain-backend/brain/utils/common_utils.py
@@ -721,7 +721,7 @@ def parse_lldpctl_output(lldpctl_output: str) -> Dict[str, Dict[str, str]]:
 
 async def collect_switch_info(ip, user, password):
     try:
-        await ensure_packages_installed(ip, user, password, ["lldpctl"])
+        await ensure_packages_installed(ip, user, password, ["lldpd"])
     except Exception:
         LOG.error("The command to ensure package lldpctl failed, "
                   "and switch information could not be retrieved")
