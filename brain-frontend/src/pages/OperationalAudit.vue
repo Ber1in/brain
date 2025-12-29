c154657c38c59f19da8448e5c16f9773ad84c723
wubl <wubl@yunsilicon.com>
2025-12-29 11:01:11 +0800

    Allow users to filter by input

--- brain-frontend/src/pages/OperationalAudit.vue
+++ brain-frontend/src/pages/OperationalAudit.vue
@@ -22,6 +22,7 @@
               v-model="filterForm.user"
               placeholder="选择用户"
               clearable
+              filterable
               style="width: 180px"
               class="user-select"
               @change="handleUserChange"
