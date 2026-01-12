f3652141b3e4863dae403aa5e1392ad107d28ac0
wubl <wubl@yunsilicon.com>
2026-01-12 16:46:44 +0800

    Change the background color of the kernel version

--- brain-frontend/src/pages/Devices/Index.vue
+++ brain-frontend/src/pages/Devices/Index.vue
@@ -299,7 +299,7 @@
                 v-if="osInfoMap[row.id!]" 
                 size="small"
                 type="info"
-                style="border: none; background-color: #ffccc6;"
+                style="border: none; background-color: #36cfc915;"
               >
                 {{ osInfoMap[row.id!].kernel_version || '-' }}
               </el-tag>
